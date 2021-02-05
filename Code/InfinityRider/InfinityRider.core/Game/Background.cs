9ec32b3a62210640d20844249f262ec79695774f
empesce <Emrick.PESCE@etu.uca.fr>
2021-02-05 17:27:49 +0100

    Modification pour que le background prenne en compte la taille de la fenêtre

--- Code/InfinityRider/InfinityRider.core/Game/Background.cs
+++ Code/InfinityRider/InfinityRider.core/Game/Background.cs
@@ -8,11 +8,15 @@ namespace InfinityRider.core.game
 {
     class Background : GameObject
     {
-        private Texture2D ImageBackground { get; set; }
+        private GraphicsDeviceManager _graphics;
+        private Game _game;
+        private int _width;
+        private int _height;
+        private Texture2D _imageBackground;
 
         public Background(Microsoft.Xna.Framework.Game game, SpriteBatch spriteBatch) : base(game, spriteBatch)
         {
-
+            _game = game;
         }
 
         public override void Initialize()
@@ -24,20 +28,42 @@ namespace InfinityRider.core.game
         {
             base.LoadContent();
 
-            ImageBackground = Game.Content.Load<Texture2D>("background/stars");
+            _imageBackground = Game.Content.Load<Texture2D>("background/stars");
 
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+
+            _width = _game.GraphicsDevice.Viewport.Width;
+            //_width = _game.Window.ClientBounds.Width;
+            _height = _game.GraphicsDevice.Viewport.Height;
+            //_height = _game.Window.ClientBounds.Height;
         }
 
         public override void Draw(GameTime gameTime)
         {
-            float width;
-            float heigth;
-            _spriteBatch.Draw(ImageBackground, new Rectangle(0,0,800,480), Color.White);
+            _spriteBatch.Draw(_imageBackground, new Rectangle(0,0,800,480), Color.White);
+
+            if(_width > 800)
+            {
+                _spriteBatch.Draw(_imageBackground, new Rectangle(800, 0, 800, 480), Color.White);
+            }
+            if (_height > 480)
+            {
+                _spriteBatch.Draw(_imageBackground, new Rectangle(0, 480, 800, 480), Color.White);
+            }
+            if (_height > 480 && _width > 800)
+            {
+                _spriteBatch.Draw(_imageBackground, new Rectangle(800, 480, 800, 480), Color.White);
+            }
+
+            //We can make the things up more extandable (by calculate modulo of width...
+            //Or do that :
+            //_spriteBatch.Draw(_imageBackground, new Rectangle(0, 0, _width, _height), Color.White);
+
+
             base.Draw(gameTime);
         }
 
